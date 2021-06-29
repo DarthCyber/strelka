8b6a0721120161a8ba20a1c23fcaa898a2cc7e43
DarthCyber <30883243+DarthCyber@users.noreply.github.com>
2021-06-29 14:35:52 -0400

    Update testingsetup.sh

--- misc/testingsetup.sh
+++ misc/testingsetup.sh
@@ -2,6 +2,21 @@
 
 #testing
 
+#/bin/sh
+#testing
+
+apt install git
+apt install net-tools
+apt install docker.io
+apt install docker-compose
+apt install docker.io
+apt install go
+apt install python
+apt install -y python3-pip
+apt install golang
+
+
 mkdir /opt/strelka/socket/
 touch /etc/docker/daemon.json
 echo '{"dns":["10.10.10.8":"8.8.8.8"]}' > /etc/docker/daemon.json
+docker pull clamav/clamav:unstable
