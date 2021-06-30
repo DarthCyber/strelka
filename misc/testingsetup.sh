d5aea1d4dd1ca0e682cce46da9cd457f517af902
DarthCyber <30883243+DarthCyber@users.noreply.github.com>
2021-06-30 13:21:34 -0400

    Update testingsetup.sh

--- misc/testingsetup.sh
+++ misc/testingsetup.sh
@@ -16,6 +16,7 @@ apt install golang
 git clone https://github.com/target/strelka.git /opt/strelka/
 
 mkdir /opt/strelka/socket/
+chmod a+rwx /opt/strelka/socket/
 touch /etc/docker/daemon.json
 echo '{"dns":["10.10.10.8":"8.8.8.8"]}' > /etc/docker/daemon.json
 docker pull clamav/clamav:unstable
